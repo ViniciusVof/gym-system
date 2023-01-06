73b7d2f52eccb30e36612f436a5263f6a9927506
Vinicius Oliveira de Freitas <vini@MacBook-Air-de-Vinicius.local>
2023-01-06 03:32:32 -0300

    Crud usuários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,9 +2,22 @@ const { response } = require('express');
 const connection = require('../data/config');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const e = require('express');
 const saltRounds = 10;
 module.exports = {
-    async listAll(req, res, next) {
+    async listOne(req, res) {
+        const { id } = req.params;
+        const user = await connection('users')
+        .where('id', id)
+        .select('*')
+        .first();
+
+            if (!user) {
+                return res.status(400).json({error: 'Nenhum usuário encontrado'});
+            }
+            return res.status(200).json(user);
+    },
+    async listAll(req, res) {
         const users = await connection('users')
             .select('*')
 
@@ -22,14 +35,51 @@ module.exports = {
         
         if(!user){
             const cryptedPassword = await bcrypt.hash(password, saltRounds);
-            const user = await connection('users').insert({
+            const newUser = await connection('users').insert({
                 fullname,
                 email,
                 password: cryptedPassword
             })
-            return res.json({fullname, email});
+            if(newUser){
+                return res.json({fullname, email});
+            } else {
+                return res.json({error: "Não foi possível cadastrar um novo usuário."})
+            }
+        } 
+        return res.status(400).json({ error: 'E-mail já cadastrado' });
+    },
+    async update(req, res) {
+        const { id } = req.params;
+        const { fullname, email, password } = req.body;
+
+        const user = await connection('users')
+        .where('id', id)
+        .first();
+
+        if (user){
+            const cryptedPassword = await bcrypt.hash(password, saltRounds);
+            const updatedUser = await connection('users').update({
+                fullname,
+                email,
+                password: cryptedPassword
+            }).where('id', id);
+            if(updatedUser){
+                return res.json({ fullname, email })
+            }
         } else {
-            return res.status(400).json({ error: 'E-mail já cadastrado' });
+            return res.json(400).json({error: "Usuário não encontrado."})
+        }
+    },
+    async delete(req, res){
+        const { id } = req.params;
+        const user = await connection('users')
+            .where('id', id)
+            .first();
+        if(user){
+            await connection('users').where('id', id).delete();
+            return res.status(204).send();
+        } else {
+            return res.status(400).json({error: "Esse usuário não existe."})
         }
     },
     async login(req, res) {
@@ -41,7 +91,7 @@ module.exports = {
 
             if (!user) {
                 return res.status(400).json({error: 'Usuário não existe.'});
-            }
+            } 
             if (await bcrypt.compare(password, user.password)){
                 const token = jwt.sign({
                     id: user.id,
--- routes.js
+++ routes.js
@@ -3,8 +3,12 @@ const UserController = require('./controllers/UserController');
 const { verifyJWT } = require('./middleware/verifyJWT');
 const routes = express.Router();
 
+// Usuários
 routes.post('/login', UserController.login);
+routes.post('/users', verifyJWT, UserController.create);
 routes.get('/users', verifyJWT, UserController.listAll);
-routes.post('/users/create', UserController.create);
+routes.get('/users/:id', verifyJWT, UserController.listOne);
+routes.put('/users/:id', verifyJWT, UserController.update);
+routes.delete('/users/:id', verifyJWT, UserController.delete);
 
 module.exports = routes;
