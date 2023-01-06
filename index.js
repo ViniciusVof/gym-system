824ed652cbc62fafe38796765228b2d6e2a71052
Vinicius Oliveira de Freitas <vini@MacBook-Air-de-Vinicius.local>
2023-01-06 01:43:29 -0300

    Ajustando .env

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const cors = require('cors');
 const routes = require('./routes.js');
 const app = express();
 
-require('dotenv').config()
+require('dotenv').config({path: './environments'});
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 app.listen(process.env.PORT);
